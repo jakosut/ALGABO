0eda93a6150ee932752e8f75c889beac4cceb0f3
nex0 <nicog710@gmail.com>
2023-09-26 20:04:24 -0300

    loading spinner

--- Obligatorio_WebApp/static/app.js
+++ Obligatorio_WebApp/static/app.js
@@ -1,104 +1,149 @@
 // Lista de ciudades de Uruguay
 const ciudadesUruguay = [
-    "Artigas", "Canelones", "Melo", "Colonia", "Durazno", "Trinidad", 
-    "Florida", "Lavalleja", "Maldonado", "Montevideo", "Paysandú", "Río Negro", 
-    "Rivera", "Rocha", "Salto", "San José", "Mercedes", "Tacuarembó", "Treinta y Tres"
+  "Artigas",
+  "Canelones",
+  "Melo",
+  "Colonia",
+  "Durazno",
+  "Trinidad",
+  "Florida",
+  "Lavalleja",
+  "Maldonado",
+  "Montevideo",
+  "Paysandú",
+  "Fray Bentos",
+  "Rivera",
+  "Rocha",
+  "Salto",
+  "San José",
+  "Mercedes",
+  "Tacuarembó",
+  "Treinta y Tres",
 ];
 
 // Llenar las opciones de ciudades
-const seleccionCiudades = document.getElementById('citiesSelection');
-ciudadesUruguay.forEach(ciudad => {
-    seleccionCiudades.innerHTML += `<label><input type="checkbox" name="cities" value="${ciudad}"> ${ciudad}</label>`;
+const seleccionCiudades = document.getElementById("citiesSelection");
+ciudadesUruguay.forEach((ciudad) => {
+  seleccionCiudades.innerHTML += `<label><input type="checkbox" name="cities" value="${ciudad}"> ${ciudad}</label>`;
 });
 
 function generarMatriz() {
-    const ciudadesSeleccionadas = [];
-    document.querySelectorAll('input[name="cities"]:checked').forEach(checkbox => {
-        ciudadesSeleccionadas.push(checkbox.value);
+  const ciudadesSeleccionadas = [];
+  document
+    .querySelectorAll('input[name="cities"]:checked')
+    .forEach((checkbox) => {
+      ciudadesSeleccionadas.push(checkbox.value);
     });
-    
-    fetch('/seleccionar-ciudades', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({selectedCities: ciudadesSeleccionadas})
+
+  // Mostrar el efecto de carga
+  mostrarSpinner();
+
+  fetch("/seleccionar-ciudades", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ selectedCities: ciudadesSeleccionadas }),
+  })
+    .then((respuesta) => respuesta.json())
+    .then((datos) => {
+      ocultarSpinner();
+      mostrarMatriz(datos.matrix, datos.selectedCities);
     })
-    .then(respuesta => respuesta.json())
-    .then(datos => {
-        mostrarMatriz(datos.matrix, datos.selectedCities);
+    .catch((error) => {
+      // Ocultar el efecto de carga en caso de error
+      ocultarSpinner();
+      console.error("Error al obtener datos:", error);
     });
 }
 
 function mostrarMatriz(matriz, ciudadesSeleccionadas) {
-    const contenedorMatriz = document.getElementById('matrixContainer');
-    let html = '<table>';
-    html += '<thead><tr><th></th>'; // Celda vacía en la esquina superior izquierda
+  const contenedorMatriz = document.getElementById("matrixContainer");
+  let html = "<table>";
+  html += "<thead><tr><th></th>"; // Celda vacía en la esquina superior izquierda
 
-    // Encabezados de la tabla
-    for (let ciudad of ciudadesSeleccionadas) {
-        html += `<th>${ciudad}</th>`;
+  // Encabezados de la tabla
+  for (let ciudad of ciudadesSeleccionadas) {
+    html += `<th>${ciudad}</th>`;
+  }
+  html += "</tr></thead><tbody>";
+
+  // Filas de la matriz
+  for (let i = 0; i < matriz.length; i++) {
+    html += `<tr><td>${ciudadesSeleccionadas[i]}</td>`; // Nombre de la ciudad
+    for (let j = 0; j < matriz[i].length; j++) {
+      html += `<td>${matriz[i][j]}</td>`;
     }
-    html += '</tr></thead><tbody>';
+    html += "</tr>";
+  }
 
-    // Filas de la matriz
-    for (let i = 0; i < matriz.length; i++) {
-        html += `<tr><td>${ciudadesSeleccionadas[i]}</td>`; // Nombre de la ciudad
-        for (let j = 0; j < matriz[i].length; j++) {
-            html += `<td>${matriz[i][j]}</td>`;
-        }
-        html += '</tr>';
-    }
+  html += "</tbody></table>";
+  contenedorMatriz.innerHTML = html;
 
-    html += '</tbody></table>';
-    contenedorMatriz.innerHTML = html;
-
-    // Mostrar el título "Matriz de Distancias:" y la selección de algoritmo
-    document.getElementById('matrixTitle').style.display = 'block';
-    document.getElementById('seleccionAlgoritmo').style.display = 'block';
+  // Mostrar el título "Matriz de Distancias:" y la selección de algoritmo
+  document.getElementById("matrixTitle").style.display = "block";
+  document.getElementById("seleccionAlgoritmo").style.display = "block";
 }
 
 function generarMapa() {
-    const algoritmoSeleccionado = document.getElementById('opcionMapa').value;
-    if (algoritmoSeleccionado === 'opcion1') {
-        fetch('/algoritmo-vecino-cercano')
-        .then(respuesta => {
-            if (!respuesta.ok) {
-                throw new Error('Error en el servidor');
-            }
-            return respuesta.json();
-        })
-        .then(datos => {
-            if (datos.error) {
-                console.error("Error del servidor:", datos.error);
-                return;
-            }
+  const algoritmoSeleccionado = document.getElementById("opcionMapa").value;
+  if (algoritmoSeleccionado === "opcion1") {
+    // Mostrar el efecto de carga
+    mostrarSpinner();
 
-            // Mostrar el camino y la distancia total en la consola
-            console.log("Camino:", datos.camino);
-            console.log("Distancia total:", datos.distanciaTotal);
+    fetch("/algoritmo-vecino-cercano")
+      .then((respuesta) => {
+        if (!respuesta.ok) {
+          throw new Error("Error en el servidor");
+        }
+        return respuesta.json();
+      })
+      .then((datos) => {
+        if (datos.error) {
+          console.error("Error del servidor:", datos.error);
+          return;
+        }
+        ocultarSpinner();
 
-            // Mostrar el camino y la distancia total en la web
-            document.getElementById('caminoResultado').innerText = "Camino: " + datos.camino.join(" -> ");
-            document.getElementById('distanciaResultado').innerText = "Distancia total: " + datos.distanciaTotal.toFixed(2) + " km";
-            document.getElementById('resultadoAlgoritmo').style.display = 'block';
+        // Mostrar el camino y la distancia total en la consola
+        console.log("Camino:", datos.camino);
+        console.log("Distancia total:", datos.distanciaTotal);
 
-            // Eliminar el iframe existente, si hay alguno
-            const existingIframe = document.querySelector('#resultadoAlgoritmo iframe');
-            if (existingIframe) {
-                existingIframe.remove();
-            }
+        // Mostrar el camino y la distancia total en la web
+        document.getElementById("caminoResultado").innerText =
+          "Camino: " + datos.camino.join(" -> ");
+        document.getElementById("distanciaResultado").innerText =
+          "Distancia total: " + datos.distanciaTotal.toFixed(2) + " km";
+        document.getElementById("resultadoAlgoritmo").style.display = "block";
 
-            // Mostrar el mapa en un nuevo iframe
-            const iframe = document.createElement('iframe');
-            iframe.src = datos.mapPath;
-            iframe.width = "100%";
-            iframe.height = "600px";
-            document.getElementById('resultadoAlgoritmo').appendChild(iframe);
-        })
-        .catch(error => {
-            console.error("Error al obtener datos:", error);
-        });
-    }
+        // Eliminar el iframe existente, si hay alguno
+        const existingIframe = document.querySelector(
+          "#resultadoAlgoritmo iframe"
+        );
+        if (existingIframe) {
+          existingIframe.remove();
+        }
+
+        // Mostrar el mapa en un nuevo iframe
+        const iframe = document.createElement("iframe");
+        iframe.src = datos.mapPath;
+        iframe.width = "100%";
+        iframe.height = "600px";
+        document.getElementById("resultadoAlgoritmo").appendChild(iframe);
+      })
+      .catch((error) => {
+        ocultarSpinner();
+        console.error("Error al obtener datos:", error);
+      });
+  }
 }
 
+function mostrarSpinner() {
+  const loadingDiv = document.getElementById("loadingDiv");
+  loadingDiv.style.setProperty("display", "flex", "important");
+}
+
+function ocultarSpinner() {
+  const loadingDiv = document.getElementById("loadingDiv");
+  loadingDiv.style.setProperty("display", "none", "important");
+}
